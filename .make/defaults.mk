53c3f37c30bf7e0ea7028cc4a5b1408a26750aad
x1ph1s <mail.x1ph1s@gmail.com>
2021-05-14 16:01:03 +0200

    Outsourced the util macros in 'util.mk'
    The util macros should be in 'util.mk', so that it is possible to
    include only the util macros and then rest. Otherwise there can be bugs
    with variables to late defined.

--- .make/defaults.mk
+++ .make/defaults.mk
@@ -1,19 +1,3 @@
-PREFIX := $(lastword $(subst /, ,$(dir $(abspath $(firstword $(MAKEFILE_LIST))))))
-MAKEFOLDER ?= .make
-
-define link
-__link_DEBUG := $$(subst %m,debug,$(1))
-__link_RELEASE := $$(subst %m,release,$(1))
-LDFLAGS := $(LDFLAGS) -l$(2)
-DEBUGLDFLAGS := $$(DEBUGLDFLAGS) -L $$(__link_DEBUG)
-RELEASELDFLAGS := $$(RELEASELDFLAGS) -L $$(__link_RELEASE)
-DEBUGDEPEND := $$(DEBUGDEPEND) $$(__link_DEBUG)/lib$(2).a
-RELEASEDEPEND := $$(RELEASEDEPEND) $$(__link_RELEASE)/lib$(2).a
-undefine __link_DEBUG
-undefine __link_RELEASE
-
-endef
-
 .PHONY:all
 all:debug
 
