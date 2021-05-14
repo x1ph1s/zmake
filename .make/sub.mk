043d38ea8063b15318a867582cec965cee401fb7
x1ph1s <mail.x1ph1s@gmail.com>
2021-05-14 16:11:08 +0200

    Make sub-make work with target all

--- .make/sub.mk
+++ .make/sub.mk
@@ -1,11 +1,12 @@
-.PHONY: all
-all: debug
-
 MAKESUBDIR = 																								\
 	@for i in $(SUBFOLDERS); do 																		\
 		echo -e "----------------------------------------\r-----------------$$i"; 		\
 		$(MAKE) -C $$i $@ --no-print-directory || break; 										\
 	done
+
+.PHONY: all
+all:
+	$(MAKESUBDIR)
 
 .PHONY: debug
 debug:
