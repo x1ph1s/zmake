c0df6c41f146c7467d44b594cb21565fa96300ea
x1ph1s <mail.x1ph1s@gmail.com>
2021-09-18 16:02:25 +0200

    Improving sub.mk terminal output

--- .make/sub.mk
+++ .make/sub.mk
@@ -1,6 +1,13 @@
 define MAKESUBDIR
 @for i in $(SUBFOLDERS); do\
-	echo -e "----------------------------------------\r-----------------$$i";\
+	let string_size=$${#i};\
+	let remain=(50-string_size);\
+	let even=remain%2;\
+	[[ $$even -eq 0 ]] && let count_before=remain/2 || let count_before=remain/2+1;\
+	let count_after=remain/2;\
+	printf %"$$count_before"s | tr " " "-";\
+	printf $$i;\
+	printf %"$$count_after"s'\n' | tr " " "-";\
 	$(MAKE) -C $$i $@ --no-print-directory || break;\
 done;\
 
